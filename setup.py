00415362cb27f2b58d3b72d4168c32489d28eebd
Scobber <scott@pep.id.au>
2021-02-24 14:12:12 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     classifiers=[  # Optional
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Cellular Automation',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
